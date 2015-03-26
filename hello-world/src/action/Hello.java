bf39827917455e670be2b9a4362c3f4eee4ccf09
Noise <noise@e-mail.jp>
2015-03-26 13:29:48 +0900

    Hello Worldを10回表示

--- hello-world/src/action/Hello.java
+++ hello-world/src/action/Hello.java
@@ -3,7 +3,7 @@ package action;
 public class Hello {
 
 	public static void main(String[] args) {
-		int[] count = {1, 2, 3, 4, 5};
+		int[] count = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 		for (int i : count) {
 			System.out.println(i + " : Hello World !!");
